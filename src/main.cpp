15fed40daa3dbb18b4e5cb96b3c1ab98836a211a
danfosterfire <danielfoster1787@gmail.com>
2024-11-28 21:27:17 -0800

    add hexefx i16 library

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include "Synth.h"
 #include <Arduino.h>
 #include <Wire.h>
-#include "effect_platereverb_F32.h"
+#include "effect_platereverb_i16.h"
 
 //#include "USBHost_t36.h"
 
@@ -22,22 +22,23 @@ int n_chorus_voice{3};
 //AudioOutputI2S           i2s1;           
 AudioOutputUSB usb0;
 //AudioEffectChorus chorus0;
-AudioEffectFreeverbStereo reverb0;
+AudioEffectPlateReverb_i16 reverb0;
 AudioAmplifier reverbSend;
 AudioConnection patchCord0(*synth->getOutput(), reverbSend);
 AudioConnection patchCord1(reverbSend, 0, reverb0, 0);
+AudioConnection patchCord2(reverbSend, 0, reverb0, 1);
 AudioMixer4 reverbMixR;
-AudioConnection patchCord2(*synth->getOutput(), 0, reverbMixR, 0);
-AudioConnection patchCord3(reverb0, 0, reverbMixR, 1);
+AudioConnection patchCord3(*synth->getOutput(), 0, reverbMixR, 0);
+AudioConnection patchCord4(reverb0, 0, reverbMixR, 1);
 AudioMixer4 reverbMixL;
-AudioConnection patchCord4(*synth->getOutput(), 0, reverbMixL, 0);
-AudioConnection patchCord5(reverb0, 1, reverbMixL, 1);
+AudioConnection patchCord5(*synth->getOutput(), 0, reverbMixL, 0);
+AudioConnection patchCord6(reverb0, 1, reverbMixL, 1);
 
 //AudioConnection patchCord1(chorus0, reverb0);
 //AudioConnection patchCord2(*synth->getOutput(), 0, usb0, 0);
 //AudioConnection patchCord1(*synth->getOutput(), 0, usb0, 1);
-AudioConnection patchCord6(reverbMixR, 0,  usb0, 0);
-AudioConnection patchCord7(reverbMixL, 0,  usb0, 1);
+AudioConnection patchCord7(reverbMixR, 0,  usb0, 0);
+AudioConnection patchCord8(reverbMixL, 0,  usb0, 1);
 
 //AudioControlSGTL5000     sgtl5000_1;
 
@@ -97,15 +98,21 @@ void setup() {
   // hmmm... bug here? https://github.com/PaulStoffregen/cores/blob/058d2808187a24e7db53803b7510e26827064c03/teensy4/usb_midi.h#L321
 
   //chorus0.begin(delayline, CHORUS_DELAY_LENGTH, n_chorus_voice);
-  reverbMixR.gain(0, 0.7);
-  reverbMixR.gain(1, 0.3);
-  reverbMixL.gain(0, 0.7);
-  reverbMixL.gain(1, 0.3);
+  reverbMixR.gain(0, 0.3);
+  reverbMixR.gain(1, 0.7);
+  reverbMixL.gain(0, 0.3);
+  reverbMixL.gain(1, 0.7);
 
-  reverbSend.gain(0.3);
+  reverbSend.gain(1.0);
 
-  reverb0.roomsize(0.5);
-  reverb0.damping(0.5);
+
+  reverb0.size(1.0);     // max reverb length
+  reverb0.lowpass(0.3);  // sets the reverb master lowpass filter
+  reverb0.lodamp(0.1);   // amount of low end loss in the reverb tail
+  reverb0.hidamp(0.2);   // amount of treble loss in the reverb tail
+  reverb0.diffusion(1.0);  // 1.0 is the detault setting, lower it to create more "echoey" reverb
+  reverb0.wet_level(1.0f);
+  reverb0.bypass_set(false);
 
   // Starting sequence
   digitalWrite(LED_BUILTIN, HIGH);
